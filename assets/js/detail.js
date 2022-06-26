29962fc76e8e00d3a3a4c0f7b38f9e661e8a5ff6
Hai <haibong2001@gmail.com>
2022-06-26 20:11:09 +0700

    commit

---
+++ assets/js/detail.js
@@ -0,0 +1,27 @@
+const btns = document.querySelectorAll(".btn");
+const iItem = document.querySelectorAll(".items");
+
+btns.forEach(button => {
+    button.addEventListener("click", () => {
+        btns.forEach(btn => btn.classList.remove('addBorder'))
+        button.classList.add('addBorder')
+        
+    })
+})
+
+
+btns.forEach((button) => {
+    button.addEventListener("click" , (e) => {
+        e.preventDefault();
+     
+        const filter = e.target.dataset.filter;
+        iItem.forEach((item => {
+            if(item.classList.contains(filter)){
+                item.style.display = "block";
+               
+            }else{
+                item.style.display = "none";
+            }
+        }))
+    })
+})
