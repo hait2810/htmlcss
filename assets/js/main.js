a489d863bb7279ba6b912860e19fc8cfb723a8c0
Hai <haibong2001@gmail.com>
2022-06-26 22:26:49 +0700

    filter item

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+
+const btns = document.querySelectorAll(".btn")
+const listItem = document.querySelectorAll(".item-list")
+
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', () => {
+//         btns.forEach(item => item.classList.remove('addClassne'))
+//         btn.classList.add('addClassne');
+//     })
+// })
+
+btns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        const filter = e.target.dataset.filter;
+        listItem.forEach((item) => {
+            if(filter == 'all'){
+                item.style.display = 'block';
+            }else {
+                if(item.classList.contains(filter)){
+                    item.style.display = 'block';
+                }else{
+                    item.style.display = 'none'
+                }
+            }
+        })
+    })
+})
